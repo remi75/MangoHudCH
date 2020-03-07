c42f66a6442087d25e3384ff0fde1201c87dcd75
jackun <jack.un@gmail.com>
2020-03-07 12:32:25 +0200

    Use XDG_DATA_HOME in opengl script if available

--- bin/run-mangohud-gl.sh
+++ bin/run-mangohud-gl.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 MANGOHUD_LIB_NAME="libMangoHud.so:libMangoHud32.so"
 if [ "$MANGOHUD_NODLSYM" = "1" ]; then
-	MANGOHUD_LIB_NAME="libmangohud_gl_nodlsym.so"
+	MANGOHUD_LIB_NAME="libMangoHud_nodlsym.so:libMangoHud_nodlsym32.so"
 fi
 
 if [ "$#" -eq 0 ]; then
@@ -13,12 +14,10 @@ if [ "$#" -eq 0 ]; then
 	exit 1
 fi
 
-# Execute the strangled program under a clean environment
-# pass through the FPS and overriden LD_PRELOAD environment variables
+# Execute the program under a clean environment
+# pass through the overriden LD_PRELOAD environment variables
 LD_PRELOAD="${LD_PRELOAD}:${MANGOHUD_LIB_NAME}"
-
-DIRNAME=$(dirname "$0")
-LD_LIBRARY_PATH="${HOME}/.local/share/MangoHud"
+LD_LIBRARY_PATH="${XDG_DATA_HOME}/MangoHud"
 
 echo $LD_LIBRARY_PATH
 exec env LD_LIBRARY_PATH="$LD_LIBRARY_PATH" LD_PRELOAD="${LD_PRELOAD}" "$@"
